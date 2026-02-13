ec7abb733221ae1539d30c3098df88267d7712f4
Александр Румянцев <a.rumyantsev@iz.ru>
2026-02-13 22:14:23 +0300

    свагер для сервера

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,13 +11,15 @@ async function bootstrap() {
     .setTitle('Notes API')
     .setDescription('The notes API description')
     .setVersion('1.0')
-    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'access-token') // Тут указываем название схемы авторизации
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'access-token')
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // Регистрируем статику для Swagger UI через Express
+  app.use('/swagger-ui', express.static(join(__dirname, '../../node_modules/swagger-ui-dist')));
+
   await app.listen(3000);
 }
-
 bootstrap();
