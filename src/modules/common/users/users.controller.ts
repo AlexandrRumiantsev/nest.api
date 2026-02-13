0e506ea885506773b5e39a371bfe8d200ec1d20d
Александр Румянцев <a.rumyantsev@iz.ru>
2026-02-13 20:53:46 +0300

    users upp

--- src/modules/common/users/users.controller.ts
+++ src/modules/common/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Delete, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiParam } from '@nestjs/swagger';
 
-@Controller('users')
+@Controller('api/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
