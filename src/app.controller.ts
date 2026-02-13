978800da0932396faeec6d05e4fca9b9d0b7ae5b
Александр Румянцев <a.rumyantsev@iz.ru>
2026-02-13 12:01:58 +0300

    Поправил роут

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { DatabaseCheckService } from './utils/database-check-service';
 
-@Controller('health')
+@Controller('api/health')
 export class AppController {
   constructor(private readonly dbCheckService: DatabaseCheckService) {}
 
